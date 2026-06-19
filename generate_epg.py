53099fb4b45f426e5503a2bbc28f7e2ce7bf2e07
babo20094-rgb <babo20094@gmail.com>
2026-06-19 08:05:42 +0200

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -1,29 +1,52 @@
 from datetime import datetime, timedelta
-import xml.etree.ElementTree as ET
 
-tree = ET.parse("Epg_365_Tage.xml")
-root = tree.getroot()
+with open("sender.txt", "r", encoding="utf-8") as f:
+    sender_liste = [zeile.strip() for zeile in f if zeile.strip()]
 
-today = datetime.now()
+xml = '<?xml version="1.0" encoding="UTF-8"?>\n<tv>\n'
 
-programmes = root.findall("programme")
+# Channels erzeugen
+for zeile in sender_liste:
+    kanal, titel, logo = [x.strip() for x in zeile.split("|", 2)]
 
-for i, programme in enumerate(programmes):
-    start = today + timedelta(days=i)
-    stop = start + timedelta(days=1)
+    xml += f'''
+<channel id="{kanal}">
+    <display-name>{kanal}</display-name>
+    <icon src="{logo}"/>
+</channel>
+'''
 
-    programme.set(
-        "start",
-        start.strftime("%Y%m%d000000 +0200")
-    )
+# Programme erzeugen
+starttag = datetime.now()
 
-    programme.set(
-        "stop",
-        stop.strftime("%Y%m%d000000 +0200")
-    )
+zeiten = [
+    (0, 6),
+    (6, 12),
+    (12, 18),
+    (18, 24)
+]
 
-tree.write(
-    "Epg_365_Tage.xml",
-    encoding="UTF-8",
-    xml_declaration=True
-)
+for zeile in sender_liste:
+    kanal, titel, logo = [x.strip() for x in zeile.split("|", 2)]
+
+    for tag in range(365):
+
+        basis = starttag + timedelta(days=tag)
+
+        for von, bis in zeiten:
+
+            start = basis.replace(hour=von, minute=0, second=0)
+            stop = basis.replace(hour=0, minute=0, second=0) + timedelta(hours=bis)
+
+            xml += f'''
+<programme start="{start.strftime("%Y%m%d%H%M%S")} +0200"
+stop="{stop.strftime("%Y%m%d%H%M%S")} +0200"
+channel="{kanal}">
+    <title>{titel}</title>
+</programme>
+'''
+
+xml += "\n</tv>"
+
+with open("Epg_365_Tage.xml", "w", encoding="utf-8") as f:
+    f.write(xml)
