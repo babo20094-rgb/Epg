ed0046ce0358e817e918a98ae677102996a4e02c
babo20094-rgb <babo20094@gmail.com>
2026-06-19 14:54:43 +0200

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -1,16 +1,15 @@
 from datetime import datetime, timedelta
 
-# Senderliste laden
+# sender.txt einlesen
 with open("sender.txt", "r", encoding="utf-8") as f:
     sender_liste = [zeile.strip() for zeile in f if zeile.strip()]
 
-xml = """<?xml version="1.0" encoding="UTF-8"?>
-<tv>
-"""
+xml = '<?xml version="1.0" encoding="UTF-8"?>\n<tv>\n'
 
-# Channels erzeugen
+# Sender erzeugen
+sender_daten = []
+
 for zeile in sender_liste:
-
     teile = [x.strip() for x in zeile.split("|")]
 
     if len(teile) < 3:
@@ -20,50 +19,37 @@ for zeile in sender_liste:
     titel = teile[1]
     logo = teile[2]
 
-    xml += f"""
-  <channel id="{kanal}">
-    <display-name>{kanal}</display-name>
-"""
+    sender_daten.append((kanal, titel))
 
-    if logo:
-        xml += f"""
+    xml += f'''
+<channel id="{kanal}">
+    <display-name>{titel}</display-name>
     <icon src="{logo}"/>
-"""
-
-    xml += """
-  </channel>
-"""
+</channel>
+'''
 
 # Programme erzeugen
 starttag = datetime.now()
 
 for tag in range(365):
 
-    start = starttag + timedelta(days=tag)
+    start = (starttag + timedelta(days=tag)).replace(
+        hour=0, minute=0, second=0
+    )
+
     stop = start + timedelta(days=1)
 
-    start_str = start.strftime("%Y%m%d") + "000000 +0200"
-    stop_str = stop.strftime("%Y%m%d") + "000000 +0200"
+    for kanal, titel in sender_daten:
 
-    for zeile in sender_liste:
-
-        teile = [x.strip() for x in zeile.split("|")]
-
-        if len(teile) < 2:
-            continue
-
-        kanal = teile[0]
-        titel = teile[1]
-
-        xml += f"""
-  <programme start="{start_str}" stop="{stop_str}" channel="{kanal}">
+        xml += f'''
+<programme start="{start.strftime('%Y%m%d%H%M%S')} +0200"
+           stop="{stop.strftime('%Y%m%d%H%M%S')} +0200"
+           channel="{kanal}">
     <title>{titel}</title>
-  </programme>
-"""
+</programme>
+'''
 
-xml += """
-</tv>
-"""
+xml += "\n</tv>"
 
 with open("Epg_365_Tage.xml", "w", encoding="utf-8") as f:
     f.write(xml)
