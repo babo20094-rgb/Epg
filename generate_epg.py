b061289ed5a15791abc374aaf5b1db6d704fa518
Jasko88 <babo20094@gmail.com>
2026-07-03 15:07:02 +0200

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -172,7 +172,39 @@ for i in range(1, 21):
 """
 
 # --------------------------------------------------
+# ----------------------------------------------------
+# Platzhalter-EPG für alle Sender aus sender.txt
+# ----------------------------------------------------
 
+jetzt = datetime.utcnow().replace(
+    hour=0,
+    minute=0,
+    second=0,
+    microsecond=0
+)
+
+for kanal, beschreibung in sender_daten:
+
+    # DYN überspringen
+    if kanal.startswith("DE| DYN PPV"):
+        continue
+
+    for stunde in range(24 * 3):   # 3 Tage
+
+        start_dummy = jetzt + timedelta(hours=stunde)
+        ende_dummy = start_dummy + timedelta(hours=1)
+
+        start_str = start_dummy.strftime("%Y%m%d%H%M%S +0000")
+        ende_str = ende_dummy.strftime("%Y%m%d%H%M%S +0000")
+
+        xml += f"""
+<programme start="{start_str}"
+           stop="{ende_str}"
+           channel="{kanal}">
+    <title>{beschreibung}</title>
+    <desc>{beschreibung}</desc>
+</programme>
+"""
 xml += "\n</tv>"
 
 with open("Epg_365_Tage.xml", "w", encoding="utf-8") as f:
