186dc09f82e45b0ceb71eecedb2e32f93cb045a1
Jasko88 <babo20094@gmail.com>
2026-07-04 04:50:05 +0200

    Update generate_epg.py

--- generate_epg.py
+++ generate_epg.py
@@ -41,9 +41,19 @@ for zeile in sender_liste:
     sender_daten.append((kanal, beschreibung))
 
     # Channel erzeugen
-    xml += f"""
+    display1 = kanal
+display2 = sendername
+display3 = sendername.title()
+display4 = sendername.lower()
+display5 = f"{sendername} HD"
+
+xml += f"""
 <channel id="{kanal}">
-    <display-name>{sendername}</display-name>
+    <display-name>{display1}</display-name>
+    <display-name>{display2}</display-name>
+    <display-name>{display3}</display-name>
+    <display-name>{display4}</display-name>
+    <display-name>{display5}</display-name>
     <icon src="{logo}"/>
 </channel>
 """
