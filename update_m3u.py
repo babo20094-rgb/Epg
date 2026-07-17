155232c04eaae635ec172dda0ba4c854d189229f
Jasko88 <babo20094@gmail.com>
2026-07-17 23:37:38 +0200

    Add initial update_m3u.py with placeholder comments
    Initial implementation of update_m3u.py with placeholder comments.

---
+++ update_m3u.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+update_m3u.py
+
+Platzhalter.
+Die vollständige Implementierung zum automatischen Ergänzen
+der tvg-id anhand von sender.txt wird hier eingefügt.
+"""
+
+print("update_m3u.py: noch keine Änderungen an der M3U durchgeführt.")
